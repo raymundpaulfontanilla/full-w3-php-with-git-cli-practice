c50e8108dffa93b726e43984a1cd045be7db0ea3
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-25 07:28:28 +0800

    mv case-sensitive to syntax.php

---
+++ case-sensitive.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Case Sensitivity</title>
+</head>
+
+<body>
+    <?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    Echo "Hello World!<br>";
+
+    $color = "red";
+    echo "My car is " .$color."<br>";
+    echo "My house is " .$COLOR ."<br>";
+    echo "My boar is ".$coLOR ."<br>";
+    ?>
+</body>
+
+</html>
