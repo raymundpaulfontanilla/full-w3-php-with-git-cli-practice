a05d9f861db2b5ecdf885d1438798cf12062f50c
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-25 20:14:22 +0800

    added get and test_get

---
+++ Superglobals/get.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$_GET</title>
+</head>
+
+<body>
+    <a href="test_get.php?subject=PHP&web=W3schools">Test $GET</a>
+</body>
+
+</html>
