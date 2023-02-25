db44f2489392ba10266b2f7bbde642da2020817b
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-26 07:04:16 +0800

    added spaces for readability

--- Forms/welcome.php
+++ Forms/welcome.php
@@ -9,8 +9,8 @@
 </head>
 
 <body>
-    Welcome <?php echo $_POST['name'];?><br>
-    Your email address is: <?php echo $_POST['email'];?><br>
+    Welcome <?php echo $_POST['name']; ?><br>
+    Your email address is: <?php echo $_POST['email']; ?><br>
 </body>
 
 </html>
