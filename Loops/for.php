90ce27fcb0efb58c8ffa304bfeda35b40ed2521e
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-25 09:42:02 +0800

    added for

---
+++ Loops/for.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+</head>
+
+<body>
+    <?php
+        for ($num1 = 0; $num1 <= 10 ; $num1++) {
+            echo "The number is: $num1 <br>";
+        }
+        echo "<br>";
+
+        for ($num2 =0 ; $num2 <= 100 ; $num2+=10) {
+            echo "The number is: $num2 <br>";
+        }
+    ?>
+</body>
+
+</html>
