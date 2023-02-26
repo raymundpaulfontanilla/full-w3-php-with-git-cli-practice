6c8683a6090dcda35aba9b82e53bdfa30bc91aa5
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-26 10:04:14 +0800

    added Gender label

--- Forms/php/form-validation.php
+++ Forms/php/form-validation.php
@@ -14,6 +14,7 @@
         E-mail: <input type="email" name="email"><br>
         Website: <input type="text" name="website"><br>
         Comment: <textarea name="comment" rows="5" cols="40"></textarea><br>
+        Gender:
         <input type="radio" name="gender" value="female">Female
         <input type="radio" name="gender" value="male">Male
         <input type="radio" name="gender" value="other">Other<br>
