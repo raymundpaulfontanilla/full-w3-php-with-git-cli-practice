d5521103e6201b5345bfb4c8b853f2f6177e88b3
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-26 10:25:28 +0800

    added space between php and span closing tag

--- Forms/php/form-required.php
+++ Forms/php/form-required.php
@@ -56,14 +56,14 @@
     ?>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-        Name: <input type="text" name="name"><span class="error">* <?php echo $nameErr; ?></span><br><br>
-        Email: <input type="email" name="email"><span class="error">* <?php echo $emailErr; ?></span><br><br>
-        Website: <input type="text" name="website"><span class="error">* <?php echo $websiteErr; ?></span><br><br>
+        Name: <input type="text" name="name"><span class="error">* <?php echo $nameErr; ?> </span><br><br>
+        Email: <input type="email" name="email"><span class="error">* <?php echo $emailErr; ?> </span><br><br>
+        Website: <input type="text" name="website"><span class="error">* <?php echo $websiteErr; ?> </span><br><br>
         Comment: <textarea name="comment" rows="5" cols="40"></textarea><br><br>
         Gender: 
         <input type="radio" name="gender" value="female">Female
         <input type="radio" name="gender" value="male">Male
-        <input type="radio" name="gender" value="other">Other<span class="error">* <?php echo $genderErr; ?></span><br><br>
+        <input type="radio" name="gender" value="other">Other<span class="error">* <?php echo $genderErr; ?> </span><br><br>
         <input type="submit" name="submit" value="Submit">
     </form>
 
