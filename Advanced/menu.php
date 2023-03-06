c7662c1cf21c5e4e16f39b16e1df17ae1408b254
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-06 17:01:22 +0800

    added menu

---
+++ Advanced/menu.php
@@ -0,0 +1,6 @@
+<?php
+    echo '<a href="https://www.w3schools.com/default.asp">Home</a> -
+    <a href="https://www.w3schools.com/html/default.asp">HTML Tutorials</a> -
+    <a href="https://www.w3schools.com/js/default.asp">JavaScript Tutorials</a> -
+    <a href="https://www.w3schools.com/php/default.asp">PHP Tutorials</a>';
+?>
