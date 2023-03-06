918e7e2b6b6457f204f21804d1f8bec9954c0651
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-06 21:59:46 +0800

    added file upload

---
+++ Advanced/file-upload.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+
+<body>
+    <form action="upload.php" method="post" enctype="multi/form-data">
+        Selec image to upload:
+        <br><br>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+</body>
+
+</html>
