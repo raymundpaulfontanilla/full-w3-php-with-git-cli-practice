74630a6f94bb203d940abea410c13668f723266b
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-08 13:36:28 +0800

    added class and objects

---
+++ OOP/classes-and-objects.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes and Objects</title>
+</head>
+
+<body>
+    <?php
+        class Fruit { // Define a class Fruit
+            // Properties
+            public $name;
+            public $color;
+
+            // Fruit Methods
+            function setName($name) {
+                $this -> name = $name;
+            }
+            function setColor($color) {
+                $this -> color = $color;
+            }
+            function getName() {
+                return $this -> name;
+            }
+            function getColor() {
+                return $this -> color;
+            }
+        }
+        // Define Objects of a Fruit class
+        $apple = new Fruit();
+        $banana = new Fruit();
+        $apple -> setName('Apple');
+        $banana -> setName('Banana');
+        $apple -> setColor('Red');
+        $banana -> setColor('Yellow');
+
+        echo "Fruit name: " . $apple -> getName() . ", " . "Fruit color: " .$apple -> getColor() . "<br>";
+        echo "Fruit name: " . $banana -> getName() . ", " . "Fruit color: " .$banana -> getColor();
+        echo "<br>";
+        var_dump($apple instanceof Fruit); // Check if an object belongs to a specific class
+    ?>
+</body>
+
+</html>
