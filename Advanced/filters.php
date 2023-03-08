373eedbd47a4a77fb9bb38358d1309e7f2072044
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-03-08 09:14:39 +0800

    added two new lines

--- Advanced/filters.php
+++ Advanced/filters.php
@@ -34,6 +34,7 @@
             echo "<br>";
 
             $int = 100;
+
             if (filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT) === false) {
                 echo "Integer is valid";
             } else {
@@ -42,6 +43,7 @@
             echo "<br>";
 
             $ip = "127.0.0.1";
+            
             if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
                 echo "$ip is a valid IP address";
             } else {
